c57d2d56652d4cdbc3115bdee974cfb59173b09c
Hamidreza89 <66661642+Hamidreza89@users.noreply.github.com>
2025-12-01 10:04:53 +0100

    Create configs.py

---
+++ configs.py
@@ -0,0 +1,12 @@
+interface Eth1
+    shutdown
+interface Eth2
+    shutdown
+interface Eth3
+    no shutdown
+    no switchport
+    ip address 10.10.1.2/30
+interface Eth4
+    no shutdown
+    no switchport
+    ip address 10.10.2.2/30
